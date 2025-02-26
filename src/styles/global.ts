083d0b1668ffe85a497fa8900292343de06acd90
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-02-26 16:01:01 -0300

    cria projeto base

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+${({ theme }) => css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${theme.color["gray-20"]};
+  }
+
+  body {
+    background-color: ${theme.color["gray-00"]};
+    color: ${theme.color.black};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  textarea,
+  button {
+    font: 400 ${theme.fontSize.sm} "Roboto", sans-serif;
+  }
+`}
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+  fontSize: {
+    xs: "16px",
+    sm: "16px",
+    md: "20px",
+  },
+  spacing: {
+    xxxs: "4px",
+    xxs: "8px",
+    xs: "16px",
+    sm: "20px",
+    md: "24px",
+    lg: "28px",
+    xl: "32px",
+    xxl: "36px",
+    xxxl: "40px",
+  },
+  color: {
+    white: "#fff",
+    black: "#1C1C1C",
+    primary: "#0500FF",
+    "gray-00": "#F0F0F0",
+    "gray-05": "#F5F5F5",
+    "gray-10": "#DFDFDF",
+    "gray-20": "#9E9E9E",
+    "blue-10": "#EDEFFB",
+  },
+} as const;
