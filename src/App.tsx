b2ebe92025ff8c44db589c91d5fcffbda9d2fa5a
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-02-27 22:26:59 -0300

    feat: adiciona hook useMediaQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { device, GlobalStyle } from "./styles/global";
 import Header from "./components/Header";
 import TitlePage from "./components/TitlePage";
 import EmployeesListDesktop from "./components/EmployeesListDesktop";
+import { useMediaQuery } from "./hooks/useMediaQuery";
 
 function App() {
+  const isMobile = useMediaQuery(device.tablet);
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header />
       <TitlePage />
-      <EmployeesListDesktop />
+      {isMobile ? <>Mobile</> : <EmployeesListDesktop />}
     </ThemeProvider>
   );
 }
