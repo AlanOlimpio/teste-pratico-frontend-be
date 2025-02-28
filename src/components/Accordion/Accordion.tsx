fd66abcfba28dbce205ffeadcb1fced9e9d48e5e
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-02-28 14:17:44 -0300

    feat: cria componente Accordion

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,62 @@
+import Chevron from "./Chevron";
+import { EmployeesProps } from "../interfaces/Employees";
+import {
+  AccordionWrapper,
+  WrapperContentAccordion,
+  Container,
+  AccordionChevron,
+  Image,
+  Wrapperbutton,
+} from "./AccordionStyles";
+import { useCallback, useRef, useState } from "react";
+
+function Accordion({
+  phone,
+  admission_date,
+  image,
+  job,
+  name,
+}: EmployeesProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const contentHeight =
+    isOpen && contentRef?.current?.clientHeight
+      ? contentRef?.current?.clientHeight + 4
+      : 0;
+  const handleToggle = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  return (
+    <AccordionWrapper>
+      <button onClick={handleToggle}>
+        <Wrapperbutton>
+          <Image src={image} alt={name} />
+          <span>{name}</span>
+        </Wrapperbutton>
+        <AccordionChevron $direction={isOpen ? "top" : "bottom"}>
+          <Chevron />
+        </AccordionChevron>
+      </button>
+
+      <WrapperContentAccordion $maxheight={contentHeight}>
+        <Container ref={contentRef}>
+          <div>
+            <strong>Cargo</strong>
+            <span>{job}</span>
+          </div>
+          <div>
+            <strong>Data de admissão</strong>
+            <span>{admission_date}</span>
+          </div>
+          <div>
+            <strong>Telefone</strong>
+            <span>{phone}</span>
+          </div>
+        </Container>
+      </WrapperContentAccordion>
+    </AccordionWrapper>
+  );
+}
+
+export default Accordion;
---
+++ src/components/Accordion/AccordionStyles.tsx
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+import { css } from "styled-components";
+
+interface ContentProps {
+  $maxheight?: number;
+}
+
+type AccordionDirectionType = "top" | "right" | "bottom" | "left";
+
+interface AccordionChevronProps {
+  $direction: AccordionDirectionType;
+}
+
+export const AccordionChevron = styled.span<AccordionChevronProps>`
+  ${({ $direction }) => css`
+    transition: all 0.25s ease-in-out;
+
+    ${$direction === "top" &&
+    css`
+      transform: rotate(-180deg);
+    `}
+    ${$direction === "right" &&
+    css`
+      transform: rotate(90deg);
+    `}
+    ${$direction === "bottom" &&
+    css`
+      transform: rotate(0deg);
+    `}
+    ${$direction === "left" &&
+    css`
+      transform: rotate(-90deg);
+    `}
+  `}
+`;
+
+export const AccordionWrapper = styled.div`
+  ${({ theme }) => css`
+    display: block;
+    box-shadow: 0 4px 4px 0px rgba(0, 0, 0, 0.25);
+    margin-bottom: 1px;
+    button {
+      width: 100%;
+      padding: calc(${theme.spacing.xxs} + ${theme.spacing.xxxs})
+        ${theme.spacing.xs};
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      background-color: ${theme.color.white};
+      cursor: pointer;
+      gap: ${theme.spacing.md};
+      border: none;
+      &:focus {
+        box-shadow: none;
+        transition: none;
+      }
+    }
+  `}
+`;
+
+export const WrapperContentAccordion = styled.div<ContentProps>`
+  ${({ $maxheight }) => css`
+    max-height: ${$maxheight}px;
+  `};
+  transition: max-height 0.25s ease-in-out;
+  overflow: hidden;
+`;
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacing.md} ${theme.spacing.xs};
+    background-color: ${theme.color.white};
+    display: grid;
+    gap: ${theme.spacing.xs};
+    div {
+      display: grid;
+      gap: ${theme.spacing.md};
+      grid-template-columns: auto auto;
+      justify-content: space-between;
+      border-style: dashed;
+      border-bottom-width: 1px;
+      border-top-width: 0;
+      border-right-width: 0;
+      border-left-width: 0;
+      border-color: ${theme.color["gray-10"]};
+      strong {
+        font-weight: bold;
+      }
+    }
+  `}
+`;
+
+export const Wrapperbutton = styled.span`
+  ${({ theme }) => css`
+    display: flex;
+    gap: ${theme.spacing.md};
+    align-items: center;
+  `}
+`;
+
+export const Image = styled.img`
+  width: 34px;
+  height: 34px;
+  border-radius: 50%;
+`;
