ead4eda9c88f412e6bd15150b08b5fda5f2edb3f
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-02-28 14:17:44 -0300

    feat: cria componente Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import TitlePage from "./components/TitlePage";
 import EmployeesListDesktop from "./components/EmployeesListDesktop";
 import { useMediaQuery } from "./hooks/useMediaQuery";
+import Accordion from "./components/Accordion";
 
 function App() {
   const isMobile = useMediaQuery(device.tablet);
@@ -14,7 +15,28 @@ function App() {
       <GlobalStyle />
       <Header />
       <TitlePage />
-      {isMobile ? <>Mobile</> : <EmployeesListDesktop />}
+      {isMobile ? (
+        <div style={{ padding: "16px" }}>
+          <Accordion
+            phone="+55 (55) 55555-555"
+            admission_date="00/00/0000"
+            id="3423412"
+            image="https://img.favpng.com/25/7/23/computer-icons-user-profile-avatar-image-png-favpng-LFqDyLRhe3PBXM0sx2LufsGFU.jpg"
+            job="Front-end"
+            name="Giovana L. Arruda"
+          />
+          <Accordion
+            phone="+55 (55) 55555-555"
+            admission_date="00/00/0000"
+            id="3423412"
+            image="https://img.favpng.com/25/7/23/computer-icons-user-profile-avatar-image-png-favpng-LFqDyLRhe3PBXM0sx2LufsGFU.jpg"
+            job="Front-end"
+            name="Giovana L. Arruda"
+          />
+        </div>
+      ) : (
+        <EmployeesListDesktop />
+      )}
     </ThemeProvider>
   );
 }
---
+++ src/components/interfaces/Employees.ts
@@ -0,0 +1,8 @@
+export interface EmployeesProps {
+  id: string;
+  name: string;
+  job: string;
+  admission_date: string;
+  phone: string;
+  image: string;
+}
